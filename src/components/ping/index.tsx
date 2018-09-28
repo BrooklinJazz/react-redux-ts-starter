9bad5644c4fc1fee25b45d2c1542e73917f9df81
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-09-27 23:19:08 -0700

    hack trick to connect mapDispatchToProps

--- src/components/ping/index.tsx
+++ src/components/ping/index.tsx
@@ -1,36 +1,33 @@
 import * as React from "react";
 import { connect } from 'react-redux'
-// import { Dispatch  } from 'redux'
+import { Dispatch  } from 'redux'
 import { IApplicationState } from 'src/store'
-// import { ping } from "src/store/ping/actions";
+import { ping } from "src/store/ping/actions";
 // import { PingActionTypes } from 'src/store/ping/types';
 // Props passed from mapStateToProps
 interface IStateProps {
     ping: boolean
 }
 
-// Props passed from mapDispatchToProps
-// interface IDispatchProps {
-//     pingIt: PingActionTypes.PING
-// }
+interface IDispatchProps {
+    pingIt(): any
+}
 
 const mapStateToProps = (state: IApplicationState) => ({
     ping: state.ping.ping,
 })
 
-// const mapDispatchToProps = (dispatch: Dispatch) => ({
-//     pingIt: () => dispatch(ping())
-// })
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    pingIt: () => dispatch(ping())
+})
 
 // Combine both state + dispatch props - as well as any props we want to pass - in a union type.
-type Props = IStateProps
+type Props = IStateProps & IDispatchProps
 
-// We separate OtherProps instead in combining them in the union type above for us to
-// use the children prop pattern without typing issues.
 class Ping extends React.Component<Props > {
     public render() {
-        return <div>PING!: {this.props.ping.toString()}</div>
+        return <div onClick={this.props.pingIt}>PING!: {this.props.ping.toString()}</div>
     }
 }
 
-export default connect(mapStateToProps, null)(Ping);
+export default connect(mapStateToProps, mapDispatchToProps)(Ping);
